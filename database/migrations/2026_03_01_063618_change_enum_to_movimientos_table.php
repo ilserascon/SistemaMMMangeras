51cf470f7cde0ce0a640c929deda8ba6ef1b3286
Ilse Rascón <ilserascon@gmail.com>
2026-02-28 23:51:38 -0700

    modificaciones a la db

---
+++ database/migrations/2026_03_01_063618_change_enum_to_movimientos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movimientos', function (Blueprint $table) {
+            $table->enum('tipo', ['Entrada', 'Traslado', 'Salida', 'Venta'])->change();
+            $table->boolean('cancelado')->default(false)->after('fecha');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movimientos', function (Blueprint $table) {
+            //
+        });
+    }
+};
