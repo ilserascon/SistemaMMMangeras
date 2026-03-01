f472741e0391dd6c907f5d406c19b4808dab500e
Ilse Rascón <ilserascon@gmail.com>
2026-02-28 20:56:39 -0700

    logo en sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,14 @@
 <div class="main-sidebar">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="{{ url('/home') }}">Logo</a>
+      <a href="{{ url('/home') }}"><img src="{{ asset('stisla/assets/img/logo_mmmangueras.jpg') }}" alt="logo" width="70%"></a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="{{ url('/home') }}"></a>
     </div>
+    <br>
     <ul class="sidebar-menu">
-      <li class="menu-header">Menú</li>
+      <li class="menu-header">&nbsp;</li>
       @if (Auth::check() && Auth::user()->role && Auth::user()->role->nombre === 'Administrador')
         <li class="{{ request()->is('admin/users*') ? 'active' : '' }}">
           <a class="nav-link" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i> <span>Usuarios</span></a>
